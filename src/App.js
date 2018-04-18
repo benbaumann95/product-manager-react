26674360149dce9269ee0f740691b75ef0348685
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-18 10:33:08 +0100

    Read products

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const products = [
+  {
+    name: 'iPad',
+    price: 200
+  },
+  {
+    name: 'iPhone',
+    price: 650
+  }
+];
+
+localStorage.setItem('products', JSON.stringify(products));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentWillMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    const products = JSON.parse(localStorage.getItem('products'));
+
+    this.setState({ products });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+
+        {this.state.products.map(product => {
+          return (
+            <div key={product.name}>
+              <span>{product.name}</span> | <span>{product.price}</span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
